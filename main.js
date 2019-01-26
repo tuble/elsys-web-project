d522a01d5dbec23c2d356a1b3909f5715afb0c3c
tuble <iana.bratoeva@gmail.com>
2019-01-26 21:23:08 +0200

    p5 testing

---
+++ main.js
@@ -0,0 +1,16 @@
+function setup() {
+
+    createCanvas(400, 400);
+
+    fill(204, 101, 192, 127);
+    stroke(127, 63, 120);
+
+
+    ellipse(200, 200, 80, 80);
+
+}
+
+/*function draw() {
+
+
+}*/
