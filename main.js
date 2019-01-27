9af298711f14c768f8de7430ea347b0c04e39705
tuble <iana.bratoeva@gmail.com>
2019-01-27 13:52:15 +0200

    sth

--- main.js
+++ main.js
@@ -1,16 +1,46 @@
-function setup() {
+var box, drum, myPart;
+var boxPat = [1,0,1,0,1,0,1,0];
+//var drumPat = [0,1,0,1,0,1,0,1];
+var msg = 'click to play';
 
-    createCanvas(400, 400);
-
-    fill(204, 101, 192, 127);
-    stroke(127, 63, 120);
-
-
-    ellipse(200, 200, 80, 80);
-
+function preload() {
+  box = loadSound('sounds/clap01.ogg');
+  //drum = loadSound('sounds/kick_hardcore01.ogg')
 }
 
-/*function draw() {
+function setup() {
+  noStroke();
+  fill(255);
+  textAlign(CENTER);
+  masterVolume(0.5);
 
+  var boxPhrase = new p5.Phrase('box', playBox, boxPat);
+  //var drumPhrase = new p5.Phrase('drum', playDrum, drumPat);
+  myPart = new p5.Part();
+  myPart.addPhrase(boxPhrase);
+  //myPart.addPhrase(drumPhrase);
+  myPart.setBPM(60);
+  //masterVolume(0.1);
+}
 
+function draw() {
+  background(0);
+  text(msg, width/2, height/2);
+}
+
+function playBox(time, playbackRate) {
+  box.rate(playbackRate);
+  box.play(time);
+}
+
+/*function playDrum(time, playbackRate) {
+  drum.rate(playbackRate);
+  drum.play(time);
 }*/
+
+function mouseClicked() {
+  if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+    myPart.start();
+    msg = 'playing part';
+  }
+} 
