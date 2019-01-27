2c7d7e57b9d7900631cfe5fff3b84abfd474c36f
tuble <iana.bratoeva@gmail.com>
2019-01-27 18:59:32 +0200

    boza

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var box, drum, myPart;
+var playing = false;
+var vol;
 
 var boxPat = [1,0,1,0,1,0,1,0];
 var drumPat = [0,0,0,0,0,0,0,0];
@@ -10,33 +12,48 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(720, 400);
-  noStroke();
-  fill(255);
-  masterVolume(0.5);
+    createCanvas(720, 400);
+    background(0);
 
-  var button = createButton("play/stop");
-  button.position(0,0);
+    noStroke();
+    fill(255);
+
     
-  var boxPhrase = new p5.Phrase('box', playBox, boxPat);
-  var drumPhrase = new p5.Phrase('drum', playDrum, drumPat);
-  myPart = new p5.Part();
-  myPart.addPhrase(boxPhrase);
-  myPart.addPhrase(drumPhrase);
-  myPart.setBPM(90);
+    var boxPhrase = new p5.Phrase('box', playBox, boxPat);
+    var drumPhrase = new p5.Phrase('drum', playDrum, drumPat);
+    myPart = new p5.Part();
+    myPart.addPhrase(boxPhrase);
+    myPart.addPhrase(drumPhrase);
+    myPart.setBPM(90);
 
 
 }
+
+console.log(vol);
+
 
 function draw() {
-  background(0);
-  myPart.start();
+    
+    
+    if(pB)
+    pB.addEventListener("click", togglePlay);
+    
+    masterVolume(0.5);
+    if(playing)
+        myPart.start();
+    else 
+        myPart.stop();
+    
+
+
+    vol = document.getElementById("volumeSlider").oninput = function() {masterVolume(this.value)};
+    
 }
 
+var pB = document.getElementById("g");
+
 function togglePlay() {
-
-
-
+    playing = !playing;
 }
 
 function playBox(time, playbackRate) {
