c477a544d9b711bb69924f0c9429c8a3ede8033e
tuble <iana.bratoeva@gmail.com>
2019-01-27 13:58:36 +0200

    a'

--- main.js
+++ main.js
@@ -1,31 +1,32 @@
 var box, drum, myPart;
+
 var boxPat = [1,0,1,0,1,0,1,0];
-//var drumPat = [0,1,0,1,0,1,0,1];
-var msg = 'click to play';
+var drumPat = [0,0,0,0,0,0,0,0];
+
 
 function preload() {
   box = loadSound('sounds/clap01.ogg');
-  //drum = loadSound('sounds/kick_hardcore01.ogg')
+  drum = loadSound('sounds/kick_hardcore01.ogg')
 }
 
 function setup() {
   noStroke();
   fill(255);
-  textAlign(CENTER);
   masterVolume(0.5);
-
+  
   var boxPhrase = new p5.Phrase('box', playBox, boxPat);
-  //var drumPhrase = new p5.Phrase('drum', playDrum, drumPat);
+  var drumPhrase = new p5.Phrase('drum', playDrum, drumPat);
   myPart = new p5.Part();
   myPart.addPhrase(boxPhrase);
-  //myPart.addPhrase(drumPhrase);
-  myPart.setBPM(60);
-  //masterVolume(0.1);
+  myPart.addPhrase(drumPhrase);
+  myPart.setBPM(90);
+
+
 }
 
 function draw() {
   background(0);
-  text(msg, width/2, height/2);
+  myPart.start();
 }
 
 function playBox(time, playbackRate) {
@@ -33,14 +34,8 @@ function playBox(time, playbackRate) {
   box.play(time);
 }
 
-/*function playDrum(time, playbackRate) {
+function playDrum(time, playbackRate) {
   drum.rate(playbackRate);
   drum.play(time);
-}*/
+}
 
-function mouseClicked() {
-  if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
-    myPart.start();
-    msg = 'playing part';
-  }
-} 
