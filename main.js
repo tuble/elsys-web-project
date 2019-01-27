edece06d20da71e69d6cb9f6924c4f1405f578c4
tuble <iana.bratoeva@gmail.com>
2019-01-27 14:19:46 +0200

    reorganizing

--- main.js
+++ main.js
@@ -10,10 +10,14 @@ function preload() {
 }
 
 function setup() {
+  createCanvas(720, 400);
   noStroke();
   fill(255);
   masterVolume(0.5);
-  
+
+  var button = createButton("play/stop");
+  button.position(0,0);
+    
   var boxPhrase = new p5.Phrase('box', playBox, boxPat);
   var drumPhrase = new p5.Phrase('drum', playDrum, drumPat);
   myPart = new p5.Part();
@@ -27,6 +31,12 @@ function setup() {
 function draw() {
   background(0);
   myPart.start();
+}
+
+function togglePlay() {
+
+
+
 }
 
 function playBox(time, playbackRate) {
