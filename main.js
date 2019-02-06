93131c8e67457c873689722568904ba4a3df5427
tuble <iana.bratoeva@gmail.com>
2019-02-06 23:07:29 +0200

    fixed timing issues + bpm text

--- main.js
+++ main.js
@@ -4,13 +4,16 @@ var default_volume = 0.5;
 var default_bpm = 80;
 var vol;
 
-var boxPat = [1,0,1,0,1,0,1,0];
-var drumPat = [0,1,0,0,0,0,0,0];
+var boxPat = [1,0,0,0,0,0,0,0];
+var drumPat = [0,0,0,0,0,0,0,0];
+
+document.getElementById("playButton").onclick = function() {togglePlay()}; 
+document.getElementById("sliderBPM").oninput = function() {changeTempo(this.value)};
 
 
 function preload() {
-  box = loadSound('sounds/clap01.ogg');
-  drum = loadSound('sounds/kick_hardcore01.ogg')
+  box = loadSound('sounds/clap01.ogg', () =>{});
+  drum = loadSound('sounds/kick_hardcore01.ogg', () =>{})
 }
 
 function setup() {
@@ -24,35 +27,30 @@ function setup() {
     fill(255);
 
     
-    var boxPhrase = new p5.Phrase('box', playBox, boxPat);
-    var drumPhrase = new p5.Phrase('drum', playDrum, drumPat);
+    //var boxPhrase = new p5.Phrase('box', playBox, boxPat);
+    //var drumPhrase = new p5.Phrase('drum', playDrum, drumPat);
+
+    var boxPhrase = new p5.Phrase('box', (time) => {box.play(time)}, boxPat);
+    var drumPhrase = new p5.Phrase('drum', (time) => {drum.play(time)}, drumPat);
     myPart = new p5.Part();
+
     myPart.addPhrase(boxPhrase);
     myPart.addPhrase(drumPhrase);
-
-
-
-
 }
 
-document.getElementById("playButton").onclick = function() {togglePlay()};
-document.getElementById("sliderBPM").oninput = function() {changeTempo(this.value)};
 
 function draw() {
-       
     if(playing)
         myPart.start();
     else 
         myPart.stop();
-
-    
 }
 
 function changeTempo(val) {
-
-    setBPM(val * 2);
+    var tempo = val * 2;
+    setBPM(tempo);
+    document.getElementById("valueBPM").textContent = tempo + " BPM";
     console.log("tempo: " + val* 2)
-
 }
 
 function togglePlay() {
@@ -60,7 +58,7 @@ function togglePlay() {
     console.log("playing set to: " + playing);
 }
 
-function playBox(time, playbackRate) {
+/*function playBox(time, playbackRate) {
   box.rate(playbackRate);
   box.play(time);
 }
@@ -68,5 +66,5 @@ function playBox(time, playbackRate) {
 function playDrum(time, playbackRate) {
   drum.rate(playbackRate);
   drum.play(time);
-}
+}*/
 
